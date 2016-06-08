f2df99c2c8685dde985c728a1525b46c3a58fad3
Armard Bellamy <armardbellamy@gmail.com>
2016-06-08 10:59:16 -0400

    Added business logic to model for calculating average reviews and updated views accordingly

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+	def format_average_stars(movie)
+		if movie.average_stars.nil?
+			content_tag(:strong, 'No reivews')
+		else
+			pluralize(movie.average_stars, 'star')
+		end
+	end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -35,4 +35,8 @@ class Movie < ApplicationRecord
   def flop?
     total_gross.blank? || total_gross < 50000000
   end
+
+  def average_stars
+    reviews.average(:stars)
+  end
 end
